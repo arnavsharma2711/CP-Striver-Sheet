ad85ab2caddec0f4f8baeb8ee3b27932a4e7c762
Arnav Sharma <arnavsharma2711@gmail.com>
2024-03-16 18:53:11 +0530

    Update 282A.cpp

--- 01. Implementation-Constructive/282A.cpp
+++ 01. Implementation-Constructive/282A.cpp
@@ -1,1 +1,32 @@
+// A. Bit++
 // https://codeforces.com/problemset/problem/282/A
+// Date: 2023-12-17
+// Solved by: Arnav Sharma
+
+#include<iostream>
+# define ll long long
+using namespace std;
+
+int solution_function(string &statement, ll &x)
+{
+  if(statement.substr(0,2) == "++" || statement.substr(1,2) == "++")
+    return ++x;
+  else if (statement.substr(0,2) == "--" || statement.substr(1,2) == "--")
+    return --x;
+  else
+    return 0;
+}
+
+int main()
+{
+  ll steps, x = 0;
+  cin>>steps;
+  while(steps--)
+  {
+    string statement;
+    cin>>statement;
+    solution_function(statement, x);
+  }
+  cout<<x<<endl;
+  return 0;
+}
