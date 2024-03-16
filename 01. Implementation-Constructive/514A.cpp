5337ad39d71a66cd6f48ccf3465b9ef504ec98ee
Arnav Sharma <arnavsharma2711@gmail.com>
2024-03-16 18:53:36 +0530

    Update 514A.cpp

--- 01. Implementation-Constructive/514A.cpp
+++ 01. Implementation-Constructive/514A.cpp
@@ -1,1 +1,37 @@
+// A. Chewbaсca and Number
 // https://codeforces.com/contest/514/problem/A
+// Date: 2023-12-27
+// Solved by: Arnav Sharma
+
+#include<iostream>
+#define ll long long
+using namespace std;
+
+string solution_function(string input)
+{
+  ll first_digit = input[0] - '0';
+  if (first_digit > 4 && first_digit != 9)
+  {
+    input[0] = 9 - first_digit + '0';
+  }
+
+  for (int i = 1; i < input.length(); i++)
+  {
+    ll current_digit = input[i] - '0';
+    if (current_digit > 4)
+    {
+      input[i] = 9 - current_digit + '0';
+    }
+  }
+
+  return input;
+}
+
+int main()
+{
+  string input;
+  cin>>input;
+  cout<<solution_function(input);
+
+  return 0;
+}
