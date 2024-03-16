838d0fcd695b9ae95bc7fea7116cebea2e6863a7
Arnav Sharma <arnavsharma2711@gmail.com>
2024-03-16 18:53:42 +0530

    Update 263A.cpp

--- 01. Implementation-Constructive/263A.cpp
+++ 01. Implementation-Constructive/263A.cpp
@@ -1,1 +1,44 @@
+// A. Beautiful Matrix
 // https://codeforces.com/problemset/problem/263/A
+// Date: 2023-12-27
+// Solved by: Arnav Sharma
+
+#include<iostream>
+#define ll long long
+using namespace std;
+
+ll solution_function(int matrix[5][5] )
+{
+  ll row, column;
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++) 
+    {
+      if (matrix[i][j] == 1)
+      {
+        row = i;
+        column = j;
+      }
+    }
+  }
+
+  return abs(row - 2) + abs(column - 2);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int matrix[5][5];
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++) 
+    {
+      cin>>matrix[i][j];
+    }
+  }
+  cout<<solution_function(matrix);
+
+  return 0;
+}
